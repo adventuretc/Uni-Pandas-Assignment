88373ab2664b0e11d9759d6588fbc6ffef106aa4
adventuretc <>
2025-03-25 12:09:28 +0100

    .

---
+++ munka.py
@@ -0,0 +1,13 @@
+
+#  ● az első feladat az legyen, hogy lebontjuk kisebb feladatokra
+● letölteni az adathalmazt és létrehozni egy Colab projektet, oda feltölteni.
+#  ● megírni az adatok betöltését.
+● megérteni a feladatot.
+● megírni ezt a kódot: Vizsgálják meg, az egyes kategóriák értékeinek eloszlását, mi hol helyezkedik el.
+	● megírni az eredményeket kiíró vagy grafikon kirajzoló kódot.
+#  ● megírni a korrelációt kereső kódot. "s hogy mi van hatással arra, hogy egy zene előrébb sorolódjon. "
+	#  ● megírni az eredményeket kiíró vagy grafikon kirajzoló kódot.
+	● szerintem ez nagyjából kész, de ellenőrizetek kéne -- B.
+● kitalálni, mi legyen a videóban.
+● megcsinálni a videót.
+● beadni az email címére. 

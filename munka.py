1f8b8159094a89356d43328d491956b027866ce8
adventuretc <>
2025-03-25 13:06:29 +0100

    .

--- munka.py
+++ munka.py
@@ -1,6 +1,23 @@
+Feladat:
+	"Készítsenek egy feltáró adatelemzést és adatvizualizálást, valamint abból információ kinyerést az alábbi három téma valamelyikében (egyet válasszanak). 3-4 fős csapatokat alkotva készítsék el a feladatot, valamint egy 5 perces videóban elemezzék és mutassák be az elkészült feladatot.
+	Töltsék fel egy drive-re és küldjék el a jakab.david@gde.hu email címre, mind a python fájl-t, mind a videót, a csoporttagok nevével és Neptun-kódjával együtt.
+
+	Aki nem vágyik kihívásra I.:
+	KSH (Központi Statisztikai Hivatal https://ksh.hu/) weboldalának használatával válasszanak egy témakört. A KSH oldaláról töltsék le a választott statisztikai adatot CSV vagy Excel formátumban. Készítsenek egy Python programot Google Colab-ban, mely legalább két minta alapján jósol meg információt. Például: a múltbeli élelmiszer árak és fizetési adatok alapján, továbbá ezek növekedésével, mikor érjük el az európai életszínvonalat?
+	
+	Aki nem vágyik kihívásra II.:
+	- https://www.kaggle.com/datasets/miquelneck/worlds-spotify-top-50-playlist-musicality-data
+	- Fent egy top 50 spotify zenét tartalmazó dataset, különböző tulajdonságokkal hozzá. Vizsgálják meg, az egyes kategóriák értékeinek eloszlását, mi hol helyezkedik el, s hogy mi van hatással arra, hogy egy zene előrébb sorolódjon.
+
+	Aki kihívásra vágyik:
+	- https://www.kaggle.com/datasets/mpwolke/cusersmarildownloadsstudents-xlsx
+	- Fent egy Japán diákok és az általuk elvégzett kurzusokról szóló data set található. A feladat, hogy a japánok által használt karakterekkel kódolt szövegek, átkódolásra kerüljenek. "
+
 
 #  ● az első feladat az legyen, hogy lebontjuk kisebb feladatokra
 ● letölteni az adathalmazt és létrehozni egy Colab projektet, oda feltölteni.
+● Az egyes piacokra vagy külön elemzést kéne futtatni, vagy ignorálni mindent ami nem a Global, szerintem. Szerintem legyen ez utóbbi, mert nincs annyira sok időnk, de ha kész akkor lehet majd még gondolkodni.
+	● megírni, hogy minden rekord ami nem Global az droppolódjon. És írjuk is ki ezt szépen szöveggel.
 #  ● megírni az adatok betöltését.
 ● megérteni a feladatot.
 ● megírni ezt a kódot: Vizsgálják meg, az egyes kategóriák értékeinek eloszlását, mi hol helyezkedik el.
